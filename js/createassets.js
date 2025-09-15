2c561971073b98f984ec0ba450c517b83f626a4b
prometheus <CobraMachete@users.noreply.github.com>
2025-09-15 12:43:53 -0400

    thumbnails for teams
    thumbnails for teams

--- js/createassets.js
+++ js/createassets.js
@@ -1144,17 +1144,48 @@ function createMulticompObj(foldEntId, prjid, teamsName) {
                                 }
         
                                 const entity = entityResponse.data[0];
+
+                                if (thumbnailwhitelist.includes(teamsName)) {
+                                    var thmbNameJoin = teamsName.toUpperCase() + "_THUMBNAIL";
+                                    session.query('select thumbnail_id from TypedContext where parent_id is "' + thumbResFold + '" and name is "' + thmbNameJoin + '"')
+                                    .then(function(data) {
+                                        console.log(data)
+
+                                        if (data.data.length > 0) {
+
+                                            var tskThumbId = data.data[0].thumbnail_id;
+
+                                            // CREATE NEW MULTICOMP
+                                            const newFold = session.create('Multicomp', {
+                                                name: teamsName,
+                                                parent_id: entity.id,
+                                                project_id: prjid,
+                                                thumbnail_id: tskThumbId
+                                            }).then(function (res) {
+                                                
+                                                resolve(res);
+                                                
+                                            });
+
+                                        } else {
+
+                                            // CREATE NEW MULTICOMP
+                                            const newFold = session.create('Multicomp', {
+                                                name: teamsName,
+                                                parent_id: entity.id,
+                                                project_id: prjid,
+                                            }).then(function (res) {
+                                                
+                                                resolve(res);
+                                                
+                                            });
+
+                                        }
+                                        
+                                    })
+                                }
         
-                                // CREATE NEW SHOT
-                                const newFold = session.create('Multicomp', {
-                                    name: teamsName,
-                                    parent_id: entity.id,
-                                    project_id: prjid,
-                                }).then(function (res) {
-                                    
-                                    resolve(res);
-                                    
-                                });
+                                
                                 
                             })
                             .catch(function (error) {
