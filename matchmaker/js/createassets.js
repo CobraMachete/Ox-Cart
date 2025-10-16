5958c39309273531458cdf9a6175a0e43e3aeeb6
prometheus <CobraMachete@users.noreply.github.com>
2025-10-16 01:39:31 -0400

    adjustments
    adjustments

--- matchmaker/js/createassets.js
+++ matchmaker/js/createassets.js
@@ -536,7 +536,8 @@ async function processRowItems(rowcollector, strucdata) {
 				const procres = await processShotItems(currrow, strucdata, SELECTED_ENTITY);
 				results.push(procres);
 				
-				currrow.riveSetBool?.('isSuccessful', true);
+				// currrow.riveSetBool?.('isSuccessful', true);
+				currrow.riveSuccess();
 				await setDetailsTxtRuntime(currrow, true);
 				currrow.riveStop?.();
 				// if your SM auto-hides the overlay, great; if not, you can call:
